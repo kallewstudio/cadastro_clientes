ef8997db01f66b9aa260b9d90fa457cb3e568c9b
kallewstudio <kallewstudio@live.com>
2024-10-28 11:47:59 -0300

    mostra no mapa onde esta localizado as empresas cadastradas

--- cadastro_clientes/cadastro_clientes/clientes/urls.py
+++ cadastro_clientes/cadastro_clientes/clientes/urls.py
@@ -1,8 +1,10 @@
 # cadastro_clientes/clientes/urls.py
 
 from django.urls import path
-from .views import cadastrar_empresa
+from .views import cadastrar_empresa, mapa_empresas
 
 urlpatterns = [
     path('cadastrar/', cadastrar_empresa, name='cadastrar_empresa'),
+    path('mapa/', mapa_empresas, name='mapa_empresas'),
+
 ]
