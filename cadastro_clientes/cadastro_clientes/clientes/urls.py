a9db2cc3721ad4244722b87c671825ec5a228d90
kallewstudio <kallewstudio@live.com>
2024-10-28 15:11:56 -0300

    Tudo funcionando e menu foi implementado

--- cadastro_clientes/cadastro_clientes/clientes/urls.py
+++ cadastro_clientes/cadastro_clientes/clientes/urls.py
@@ -1,10 +1,11 @@
 # cadastro_clientes/clientes/urls.py
 
 from django.urls import path
-from .views import cadastrar_empresa, mapa_empresas
+from .views import cadastrar_empresa, mapa_empresas, listar_empresas
 
 urlpatterns = [
     path('cadastrar/', cadastrar_empresa, name='cadastrar_empresa'),
+    path('listar/', listar_empresas, name='listar_empresas'),
     path('mapa/', mapa_empresas, name='mapa_empresas'),
 
 ]
